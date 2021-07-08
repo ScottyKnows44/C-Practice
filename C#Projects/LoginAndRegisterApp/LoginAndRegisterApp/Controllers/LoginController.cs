a8bf7ba131a9850cc7e07bce4839e4b4096f8c3f
ScottyKnows44 <scottcfalcon@gmail.com>
2021-07-08 16:28:11 -0700

    C# project part 1 finished

---
+++ C#Projects/LoginAndRegisterApp/LoginAndRegisterApp/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using LoginAndRegisterApp.Models;
+using LoginAndRegisterApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginAndRegisterApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel) 
+        {
+            SecurityService security = new SecurityService();
+
+            if (security.IsValid(userModel))
+            {
+                return View("LoginSuccess", userModel);
+            }
+            else {
+                return View("LoginFailure", userModel);
+            }
+                  
+        }
+    }
+}
---
+++ C#Projects/LoginAndRegisterApp/LoginAndRegisterApp/Services/SecurityService.cs
@@ -0,0 +1,26 @@
+﻿using LoginAndRegisterApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginAndRegisterApp.Services
+{
+    public class SecurityService
+    {
+
+  
+        usersDAO usersDAO = new usersDAO();
+
+        public SecurityService()
+        {
+          
+        }
+        public bool IsValid(UserModel user) 
+        {
+            return usersDAO.FindUserByNameAndPassword(user);
+         
+        }
+
+    }
+}
