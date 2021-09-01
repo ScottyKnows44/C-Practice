5029f618fcf34319e3b4deec930f97f124055ebc
ScottyKnows44 <scottcfalcon@gmail.com>
2021-08-31 22:50:37 -0700

    C# practice

--- ErrorPractice/ErrorPractice/Program.cs
+++ ErrorPractice/ErrorPractice/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Text.RegularExpressions;
-
+using System.Collections.Generic;
 namespace ErrorPractice
 {
     class Program
@@ -9,12 +9,34 @@ namespace ErrorPractice
         static void Main(string[] args)
         {
             string s = "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog";
-            Console.WriteLine(SumOfIntegersInString(s));
+            SumOfIntegersInString(s);
+        IsPalindrome(44333344);
+            IsIsogram("Consecutive");
         }
 
         public static int SumOfIntegersInString(string s)
         {
             return Regex.Matches(s, "\\d+").Sum(x => int.Parse(x.Value));
+        }
+        public static bool IsPalindrome(int num)
+        {
+            string reverse = string.Concat(num.ToString().Reverse());
+
+            return num.ToString() == reverse;
+        }
+
+        public static bool IsIsogram(string str)
+        {
+            char[] array = str.ToLower().ToCharArray();
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            foreach(char x in array) {
+                Console.WriteLine(x);
+                if(map.ContainsKey(x)) {
+                    return false;
+                }
+                map.Add(x, 1);
+            }
+            return true;
         }
     }
 }
